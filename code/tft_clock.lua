cb260f32a8f5f28274e538b24b3ef5fc88005c94
Divo <divoapple@gmail.com>
2016-11-25 23:49:41 +0000

    Use libs over copy-pasta functions

--- code/tft_clock.lua
+++ code/tft_clock.lua
@@ -45,42 +45,22 @@ tz=1
 udptimer = 2
 udptimeout=1000
 
-
 function getTime()
-	local request=string.char( 227, 0, 6, 236, 0,0,0,0,0,0,0,0, 49, 78, 49, 52,
-		0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-		)
-		socket = net.createConnection(net.UDP, 0)
-		socket:on("receive", function(sock, payload)
-			sock:close()
-			print(payload)
-			lastSync = calcStamp(payload:sub(41, 44))
-			setTime(lastSync)
-			--add callback?
-			
-			collectgarbage()
-		end)
-		socket:connect(123,ntpServer)
-		tmr.alarm(udptimer, udptimeout,0, function() socket:close() end)
-		socket:send(request)
-end	
-
-
-function calcStamp(bytes)
-	local highw, loww, ntpstamp
-	highw = bytes:byte(1) * 256 + bytes:byte(2)
-	loww = bytes:byte(3) * 256 + bytes:byte(4)
-	ntpstamp=( highw * 65536 + loww) + ( tz * 3600 ) --ntp stamp, seconds since 1.1.1900
-	ustamp=ntpstamp - 1104494400 - 1104494400		 --unix stamp, since 1.1.1970
-	return ustamp
+	sntp.sync(ntpServer),
+		function(sec, usec, server)
+			clearTime(timeString(hour, minute, second))
+			hour,minue,second = convertTime(sec)
+			drawTime(timeString(hour, minute, second))
+		end
 end
 
-function setTime(ts)
-	clearTime(timeString(hour, minute, second))
-	hour = ts % 86400 / 3600
-	minute = ts % 3600 / 60
-	second = ts % 60
-	drawTime(timeString(hour, minute, second))
+
+
+function convertTime(sec)
+	local hour = sec % 86400 / 3600
+	local minute = sec % 3600 / 60
+	local second = sec % 60
+	return hour, minute, second
 end
 
 function timeString(h,m,s)
