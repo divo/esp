8e52a17e7cf1d24dfcc41882c2e0587c6005dcf0
divo <divoapple@gmail.com>
2022-11-17 21:38:05 +0000

    Update flash command for OSX

--- flash_with_firmware.sh
+++ flash_with_firmware.sh
@@ -1,1 +1,1 @@
-esptool.py --port /dev/ttyUSB0 write_flash -fm dio -fs 32m 0x00000 ./nodemcu-28-10-16-integer-18.bin  0x3fc000 esp_init_data_default.bin
+esptool.py --port /dev/tty.usbserial-140 write_flash -fm dio -fs detect 0x00000 ./firmware/nodemcu-28-10-16-integer-18.bin  0x3fc000 ./firmware/esp_init_data_default.bin
