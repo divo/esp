bd1dfa896eef69f37fc8d999d668571a7df6ba33
divo <divoapple@gmail.com>
2022-11-19 21:17:30 +0000

    Skip connecting to wifi while I debug this

--- code/init.lua
+++ code/init.lua
@@ -62,12 +62,12 @@ wifi_disconnect_event = function(T)
 end
 
 -- Register WiFi Station event callbacks
-wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, wifi_connect_event)
-wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, wifi_got_ip_event)
-wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, wifi_disconnect_event)
-
-print("Connecting to WiFi access point...")
-wifi.setmode(wifi.STATION)
-wifi.sta.config({ssid=SSID, pwd=PASSWORD})
+-- wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, wifi_connect_event)
+-- wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, wifi_got_ip_event)
+-- wifi.eventmon.register(wifi.eventmon.STA_DISCONNECTED, wifi_disconnect_event)
+-- 
+-- print("Connecting to WiFi access point...")
+-- wifi.setmode(wifi.STATION)
+-- wifi.sta.config({ssid=SSID, pwd=PASSWORD})
 -- wifi.sta.connect() not necessary because config() uses auto-connect=true by default
-
+startup()
